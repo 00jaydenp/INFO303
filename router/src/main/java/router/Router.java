ad36630739a3c480c3dae890629ab5b9f9f5827b
praja126 <praja126@student.otago.ac.nz>
2022-05-16 15:41:49 +1200

    add Router Config code

--- router/src/main/java/router/Router.java
+++ router/src/main/java/router/Router.java
@@ -1,9 +1,30 @@
 package router;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.activemq.ActiveMQComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
 public class Router {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        // create default context
+        CamelContext camel = new DefaultCamelContext();
 
-	}
+// register ActiveMQ as the JMS handler
+        ActiveMQComponent activemq = ActiveMQComponent.activeMQComponent();
+        camel.addComponent("jms", activemq);
+
+// transfer the entire exchange, or just the body and headers?
+        activemq.setTransferExchange(false);
+
+// trust all classes being used to send serialised domain objects
+        activemq.setTrustAllPackages(true);
+
+// turn exchange tracing on or off (false is off)
+        camel.setTracing(false);
+
+// enable stream caching so that things like loggers don't consume the messages
+        camel.setStreamCaching(true);
+    }
 
 }
